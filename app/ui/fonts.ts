36b157da64886c9a526354fe7280c3fa97e5aac7
Shawn P <shawn@ais.intranet>
2024-11-11 12:40:17 +0530

    initial commit :zap:

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Roboto } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "400",
+});
